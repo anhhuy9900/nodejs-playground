921edde91bed5d0a4c571c93a8d718a441a302b0
be-anhhuy <huy.nguyen@enotion.io>
2024-08-09 18:22:11 +0700

    Practice async package, and timeout requet

---
+++ src/async-package/mapLimit.ts
@@ -0,0 +1,20 @@
+import async from "async";
+import axios from 'axios';
+
+// ...or ES2017 async functions
+const urls = [
+    'https://my-json-server.typicode.com/typicode/demo/comments',
+    'https://dummyjson.com/products?delay=2000',
+    'https://dummyapi.io/data/v1/user?limit=100',
+    'https://reqres.in/api/users',
+    'https://fakestoreapi.com/carts'
+]
+async.mapLimit(urls, 5, async function(url: string) {
+    const response = await fetch(url)
+    console.log(`URL: ${url}, statusText:${response.statusText}`)
+}, (err, results) => {
+    if (err) throw err
+    // results is now an array of the response bodies
+    console.log(results)
+})
+
---
+++ src/timeout-requets/index.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+// Middleware to set request timeout
+const requestTimeout = (duration: number) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const timer = setTimeout(() => {
+            if (!res.headersSent) {
+                console.log('Request timeout -> url: %s', req.url);
+                res.status(408).send('Request Timeout');
+            }
+        }, duration);
+
+        // Clear the timer if the response is sent before the timeout
+        const clearTimer = () => clearTimeout(timer);
+
+        res.on('finish', clearTimer);
+        res.on('close', clearTimer);
+
+        next();
+    };
+};
+
+// app.use(requestTimeout);
+
+app.get('/quick', requestTimeout(2000), (req: Request, res: Response) => {
+
+    setTimeout(() => {
+        res.send('Quick response!');
+    }, 1000);
+});
+
+app.get('/slow', requestTimeout(5000), (req: Request, res: Response) => {
+    setTimeout(() => {
+        if (!res.headersSent) {
+            res.send('Slow response!');
+        }
+    }, 8000);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
