476ec7fbab1d5913d610e008a4e2cc8a52b7725c
huynguyen <huynguyen@freec.asia>
2025-03-16 11:36:28 +0700

    Add file config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+};
